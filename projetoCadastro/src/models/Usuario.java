efe93ea7c01b5b73be50e8f5031f24a8fcea50ec
psicoder0101 <pedrocsouza97@gmail.com>
2024-10-01 13:09:15 -0300

    created model Usuario

---
+++ projetoCadastro/src/models/Usuario.java
@@ -0,0 +1,43 @@
+package models;
+
+public class Usuario {
+
+    private String nome;
+    private String cpf;
+    private int idade;
+
+    public Usuario (String nome, String cpf, int idade){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return STR."\"\{cpf}\",\"\{nome}\",\"\{idade}\"";
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
